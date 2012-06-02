c16f7580a70111524ac7def552967f63c87bc23b
Luciano Resende <lresende@apache.org>
2012-06-02 23:34:04 +0000

    PHOTARK-80 - partially submitting the may 28th patch from Prabhath (except bin and gen folders)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/photark/mobile/trunk@1345605 13f79535-47bb-0310-9956-ffa450edef68

---
+++ assets/www/scripts/contacts.js
@@ -0,0 +1,67 @@
+//////////////////////////////////////////////
+function getContacts() {
+	var optFilter = new ContactFindOptions();
+	optFilter.filter = "";
+	// to return all contacts
+	optFilter.multiple = true;
+	// return multiple results
+	fields = [ "name" ];
+
+	// get all contacts
+	navigator.contacts.find(fields, gcsSuccess, gcsError, optFilter);
+}
+
+/* get all contacts error */
+function gcsError(contactError) {
+	alert('Contacts Error');
+}
+
+function gcsSuccess(contacts) {
+	alert('loaded');
+	if (contacts.length != 0) {
+		// get formatted names and sort
+		var names = new Array();
+		for ( var i = 0; i < contacts.length; ++i) {
+			if (contacts[i].name) {
+				if (contacts[i].name.formatted)
+					names.push(contacts[i].name.formatted);
+			}
+		}
+		names.sort();
+
+		var list = $('#allContacts');
+		// list to put contacts
+		list.html('');
+		// remove all the list items
+		var divider = names[0][0];
+		// first divider letter
+		for ( var i = 0; i < names.length; ++i) {
+			if (divider != names[i][0]) {// add a new divider
+				divider = names[i][0];
+				list
+						.append('<li data-role="list-divider">' + divider
+								+ '</li>');
+				list
+						.append('<li><a href="#EditMetadata"><img src="img/contactIcon.png" class="ui-li-icon" alt="Contact" onclick="onContactsClick()"/>'
+								+ names[i] + '</a></li>');
+			} else {
+				if (i == 0)
+					list.append('<li data-role="list-divider">' + divider
+							+ '</li>');
+				list
+						.append('<li><a href="#EditMetadata"><img src="img/contactIcon.png" class="ui-li-icon" alt="Contact" onclick="onContactsClick()"/>'
+								+ names[i] + '</a></li>');
+			}
+		}
+
+		list.listview('refresh');
+		// refresh the list view
+	} else
+		$('#allContacts').html('No contacts');
+}
+
+function onContactsClick(name){
+	$('#editPeople').append(name+',');
+}
+
+// ////////////////////////////////////////////////////////////////
---
+++ src/org/apache/photark/PhotarkMobileActivity.java
@@ -0,0 +1,18 @@
+package org.apache.photark;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class PhotarkMobileActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	super.loadUrl("file:///android_asset/www/index.html");
+    }
+    
+   
+
+	
+}
